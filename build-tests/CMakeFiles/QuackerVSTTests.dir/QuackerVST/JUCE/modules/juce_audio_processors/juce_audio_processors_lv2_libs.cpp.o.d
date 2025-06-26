65fb46e5d07d09064768702c1042c8f04dd53ac7
dhbusiness <deividshvostovsbusiness@gmail.com>
2025-06-26 12:54:53 +0100

    Removed lots from previous attempt at unit testing

--- build-tests/CMakeFiles/QuackerVSTTests.dir/QuackerVST/JUCE/modules/juce_audio_processors/juce_audio_processors_lv2_libs.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/QuackerVSTTests.dir/QuackerVST/JUCE/modules/juce_audio_processors/juce_audio_processors_lv2_libs.cpp.o: \
-  /Users/deividshvostovs/Desktop/Coding/PluginQuacker/QuackerVST/JUCE/modules/juce_audio_processors/juce_audio_processors_lv2_libs.cpp
